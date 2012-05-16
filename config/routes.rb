4ecc769bcf7ab53ac6cdfca20d2f8b0415c98e51
Adam Weis <aweis@andrew.cmu.edu>
2012-05-16 05:43:13 -0400

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Locoapp::Application.routes.draw do
 
   resources :schools
 
+  root :to => 'schools#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,7 +53,6 @@ Locoapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
