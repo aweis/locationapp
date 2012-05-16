e9af8accf3a57f919b021fc63992575df2ee023f
Adam Weis <aweis@andrew.cmu.edu>
2012-05-16 05:25:53 -0400

    10

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -3,4 +3,18 @@ class Attraction < ActiveRecord::Base
   
   belongs_to :school
 
+  before_save :find_attraction_coordinates
+
+  private
+  
+  def find_attraction_coordinates
+    coord = Geokit::Geocoders::GoogleGeocoder.geocode "#{name}, #{street}"
+    if coord.success
+      self.lat, self.lon = coord.ll.split(",")
+    else
+      errors[:base] << "Error with geocoding"
+    end
+  end
+
+
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,11 +1,25 @@
 class School < ActiveRecord::Base
   attr_accessible :description, :lat, :lon, :name, :state, :attractions_attributes
 
-  has_many :attractions
+  has_many :attractions, :dependent => :destroy
+
+  before_save :find_school_coordinates
 
   accepts_nested_attributes_for :attractions, :reject_if => lambda { |attraction| attraction[:name].blank? }
   
   
   STATES_LIST = [['Alabama', 'AL'],['Alaska', 'AK'],['Arizona', 'AZ'],['Arkansas', 'AR'],['California', 'CA'],['Colorado', 'CO'],['Connectict', 'CT'],['Delaware', 'DE'],['District of Columbia ', 'DC'],['Florida', 'FL'],['Georgia', 'GA'],['Hawaii', 'HI'],['Idaho', 'ID'],['Illinois', 'IL'],['Indiana', 'IN'],['Iowa', 'IA'],['Kansas', 'KS'],['Kentucky', 'KY'],['Louisiana', 'LA'],['Maine', 'ME'],['Maryland', 'MD'],['Massachusetts', 'MA'],['Michigan', 'MI'],['Minnesota', 'MN'],['Mississippi', 'MS'],['Missouri', 'MO'],['Montana', 'MT'],['Nebraska', 'NE'],['Nevada', 'NV'],['New Hampshire', 'NH'],['New Jersey', 'NJ'],['New Mexico', 'NM'],['New York', 'NY'],['North Carolina','NC'],['North Dakota', 'ND'],['Ohio', 'OH'],['Oklahoma', 'OK'],['Oregon', 'OR'],['Pennsylvania', 'PA'],['Rhode Island', 'RI'],['South Carolina', 'SC'],['South Dakota', 'SD'],['Tennessee', 'TN'],['Texas', 'TX'],['Utah', 'UT'],['Vermont', 'VT'],['Virginia', 'VA'],['Washington', 'WA'],['West Virginia', 'WV'],['Wisconsin ', 'WI'],['Wyoming', 'WY']]
+
+private
+
+  def find_school_coordinates
+    coord = Geokit::Geocoders::GoogleGeocoder.geocode "#{name}, #{state}"
+    if coord.success
+      self.lat, self.lon = coord.ll.split(",")
+    else
+      errors[:base] << "Error with geocoding"
+    end
+  end
+
 
 end
